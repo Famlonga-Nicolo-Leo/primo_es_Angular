538a9a84f4eadb2c168d4cf0edeeef1c3d0f4e5a
Famlonga-Nicolo-Leo <famlonga.nicolo@iisgalvanimi.edu.it>
2025-10-02 08:09:26 +0000

    ciao works

---
+++ primo-progetto/src/app/ciao/ciao.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ciao',
+  imports: [],
+  templateUrl: './ciao.html',
+  styleUrl: './ciao.css',
+  standalone: true
+})
+export class Ciao {
+
+}
